4f5dda50b106f2b634376712ed587a3fa4c36635
PeterDauwe <bobo5290461@gmail.com>
2021-11-21 14:12:17 +0100

    first upload

---
+++ scripts/wal.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+### Check if python-pywal is installed
+### Check if dir exists and copy the template in its proper place
+if [ -x "$(command -v wal)" ]; then
+  if [ ! -d $HOME/.config/wal/templates ] ; then
+    mkdir $HOME/.config/wal/templates &&
+    cp $HOME/.config/leftwm/themes/current/template-wal/color.leftwm-theme.toml  $HOME/.config/wal/templates
+  elif
+    [ ! -f $HOME/.config/wal/templates/color.leftwm-theme.toml ] ; then
+        cp -f $HOME/.config/leftwm/themes/current/template-wal/color.leftwm-theme.toml  $HOME/.config/wal/templates
+  fi
+    my_array=(/usr/share/backgrounds/*)
+    wal -t -e -i ${my_array[$(( $RANDOM % ${#my_array[@]}))]}
+  ##  cat ~/.cache/wal/color.leftwm-theme.toml &&
+    bat ~/.cache/wal/color.leftwm-theme.toml &&               
+    cp -f ~/.cache/wal/color.leftwm-theme.toml $HOME/.config/leftwm/themes/current/theme.toml &&
+    leftwm command "LoadTheme $HOME/.config/leftwm/themes/current/theme.toml"
+fi
